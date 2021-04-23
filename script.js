1d40321c9b84d3292f00809b06ceff0585c6b8d2
studzin-sky <the.stucc@gmail.com>
2021-04-23 16:08:22 +0200

    first consts

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const equalsButton = document.querySelector('[data-equals]');
+const clearButton = document.querySelector('[data-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+const currentOperation = document.querySelector('[data-current-operation]');
+const previousOperation = document.querySelector('[data-previous-operation]');
